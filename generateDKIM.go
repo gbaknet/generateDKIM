d8f0c039a1b6ca6d76e9170b68427e704e2e9b00
gbaknet <ozcanbicak@gmail.com>
2017-04-05 12:26:33 +0000

    Update generateDKIM.go

--- generateDKIM.go
+++ generateDKIM.go
@@ -5,20 +5,21 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	b64 "encoding/base64"
-	"encoding/pem"
 )
 
 func DKIM() (pri, pub string) {
 	privatekey, err := rsa.GenerateKey(rand.Reader, 1024)
+	pub = ""
+	pri = ""
 	if err == nil {
 		var publickey *rsa.PublicKey
 		publickey = &privatekey.PublicKey
 		Priv := x509.MarshalPKCS1PrivateKey(privatekey)
-		privBytes := pem.EncodeToMemory(&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: Priv,
-		})
-		privkey := string(privBytes)
+		// privBytes := pem.EncodeToMemory(&pem.Block{
+		// 	Type:  "RSA PRIVATE KEY",
+		// 	Bytes: Priv,
+		// })
+		//privkey := string(privBytes)
 		privkeyplain := b64.StdEncoding.EncodeToString([]byte(Priv))
 		// fmt.Println(Priv)
 		// fmt.Println()
@@ -28,11 +29,11 @@ func DKIM() (pri, pub string) {
 		pri = privkeyplain
 		Pub, err := x509.MarshalPKIXPublicKey(publickey)
 		if err == nil {
-			pubBytes := pem.EncodeToMemory(&pem.Block{
-				Type:  "RSA PUBLIC KEY",
-				Bytes: Pub,
-			})
-			pubkey := string(pubBytes)
+			// pubBytes := pem.EncodeToMemory(&pem.Block{
+			// 	Type:  "RSA PUBLIC KEY",
+			// 	Bytes: Pub,
+			// })
+			//pubkey := string(pubBytes)
 			pubkeyplain := b64.StdEncoding.EncodeToString([]byte(Pub))
 			// fmt.Println(Pub)
 			// fmt.Println()
@@ -40,6 +41,8 @@ func DKIM() (pri, pub string) {
 			// fmt.Println()
 			// fmt.Println(pubkey)
 			pub = pubkeyplain
+
 		}
 	}
+	return
 }
