3d331489a2a2f8d0f3b5dffbcd070beb2a2b3320
gbaknet <ozcanbicak@gmail.com>
2017-04-05 12:20:46 +0000

    Update generateDKIM.go

--- generateDKIM.go
+++ generateDKIM.go
@@ -1,45 +1,46 @@
 package GenerateDKIM
 
 import (
-	cryptorand
-	cryptorsa
-	cryptox509
-	b64 encodingbase64
-	encodingpem
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	b64 "encoding/base64"
+	"encoding/pem"
+	"fmt"
 )
 
-func GenerateDKIM() (pri, pub string) {
-	privatekey, err = rsa.GenerateKey(rand.Reader, 1024)
+func GenerateDKIM()(pri,pub string) {
+	privatekey, err := rsa.GenerateKey(rand.Reader, 1024)
 	if err == nil {
-		var publickey rsa.PublicKey
+		var publickey *rsa.PublicKey
 		publickey = &privatekey.PublicKey
-		Priv = x509.MarshalPKCS1PrivateKey(privatekey)
-		privBytes = pem.EncodeToMemory(&pem.Block{
-			Type  RSA PRIVATE KEY,
-			Bytes Priv,
+		Priv := x509.MarshalPKCS1PrivateKey(privatekey)
+		privBytes := pem.EncodeToMemory(&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: Priv,
 		})
-		privkey = string(privBytes)
-		privkeyplain = b64.StdEncoding.EncodeToString([]byte(Priv))
-		 fmt.Println(Priv)
-		 fmt.Println()
-		 fmt.Println(privkeyplain)
-		 fmt.Println()
-		 fmt.Println(privkey)
-		pri = privkeyplain
-		Pub, err = x509.MarshalPKIXPublicKey(publickey)
+		privkey := string(privBytes)
+		privkeyplain := b64.StdEncoding.EncodeToString([]byte(Priv))
+		// fmt.Println(Priv)
+		// fmt.Println()
+		// fmt.Println(privkeyplain)
+		// fmt.Println()
+		// fmt.Println(privkey)
+		pri=privkeyplain
+		Pub, err := x509.MarshalPKIXPublicKey(publickey)
 		if err == nil {
-			pubBytes = pem.EncodeToMemory(&pem.Block{
-				Type  RSA PUBLIC KEY,
-				Bytes Pub,
+			pubBytes := pem.EncodeToMemory(&pem.Block{
+				Type:  "RSA PUBLIC KEY",
+				Bytes: Pub,
 			})
-			pubkey = string(pubBytes)
-			pubkeyplain = b64.StdEncoding.EncodeToString([]byte(Pub))
-			 fmt.Println(Pub)
-			 fmt.Println()
-			 fmt.Println(pubkeyplain)
-			 fmt.Println()
-			 fmt.Println(pubkey)
-			pub = pubkeyplain
+			pubkey := string(pubBytes)
+			pubkeyplain := b64.StdEncoding.EncodeToString([]byte(Pub))
+			// fmt.Println(Pub)
+			// fmt.Println()
+			// fmt.Println(pubkeyplain)
+			// fmt.Println()
+			// fmt.Println(pubkey)
+			pub=pubkeyplain
 		}
 	}
 }
